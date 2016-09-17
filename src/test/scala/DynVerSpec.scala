9dce2250673a4f8cb7d941279bec0aee1c9b7ada
Dale Wijnand <dale.wijnand@gmail.com>
2016-09-17 14:51:21 +0100

    Rename to DynVer

--- src/test/scala/DynVerSpec.scala
+++ src/test/scala/DynVerSpec.scala
@@ -1,6 +1,6 @@
 import org.scalacheck._, Prop._
 
-object DynverSpec extends Properties("DynverSpec") {
+object DynVerSpec extends Properties("DynVerSpec") {
   property("when on v1.0.0 tag, w/o local changes") = passed
   property("when on v1.0.0 tag with local changes") = passed
   property("when on commit 1234abcd: 3 commits after v1.0.0 tag, w/o local changes") = passed
