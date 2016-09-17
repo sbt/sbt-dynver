43a2def3704b8bcbf66b9771474a3c3177bc1a08
Dale Wijnand <dale.wijnand@gmail.com>
2016-09-17 15:34:01 +0100

    Add types and use sys.props#get

--- src/main/scala/sbtdynver/DynVerPlugin.scala
+++ src/main/scala/sbtdynver/DynVerPlugin.scala
@@ -17,27 +17,27 @@ object DynverPlugin extends AutoPlugin {
 }
 
 final case class DynVer(wd: Option[File], clock: Clock) {
-  def version() = {
-    def overrideVersion = Option(sys props "project.version")
+  def version(): String = {
+    def overrideVersion = sys.props get "project.version"
     def   dynverVersion = Some(makeDynVer())
     def    datedVersion = s"HEAD+$currentYearMonthDay"
 
     Seq(overrideVersion, dynverVersion) reduce (_ orElse _) getOrElse datedVersion
   }
 
-  def isSnapshot() = isDirty() || hasNoTags()
+  def isSnapshot(): Boolean = isDirty() || hasNoTags()
 
-  def currentYearMonthDay() = "%1$tY%1$tm%1$td" format new Date
+  def currentYearMonthDay(): String = "%1$tY%1$tm%1$td" format new Date
 
-  def makeDynVer() = {
+  def makeDynVer(): String = {
     Process(s"""git describe --abbrev=8 --match v[0-9].* --always --dirty=+$currentYearMonthDay""", wd).!!.init
       .replaceAll("^v", "")
       .replaceAll("-([0-9]+)-g([0-9a-f]{8})", "+$1-$2")
   }
 
-  def isDirty() = Process("git status --untracked-files=no --porcelain", wd).!!.nonEmpty
+  def isDirty(): Boolean = Process("git status --untracked-files=no --porcelain", wd).!!.nonEmpty
 
-  def hasNoTags() = Process("git for-each-ref --format %(objecttype) refs/tags/", wd).!!
+  def hasNoTags(): Boolean = Process("git for-each-ref --format %(objecttype) refs/tags/", wd).!!
     .linesIterator.forall(_ startsWith "commit")
 }
 
