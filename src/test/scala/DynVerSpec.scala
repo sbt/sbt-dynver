758a330ab6ae824b10a8ce8f547b8686fbf0a89e
Dale Wijnand <dale.wijnand@gmail.com>
2016-09-17 15:02:40 +0100

    Add tag clean test

--- src/test/scala/DynVerSpec.scala
+++ src/test/scala/DynVerSpec.scala
@@ -1,7 +1,14 @@
+import java.nio.file._, StandardOpenOption._
+
+import scala.collection.JavaConverters._
+
 import org.scalacheck._, Prop._
+import org.eclipse.jgit.api._
+
+import sbtdynver._
 
 object DynVerSpec extends Properties("DynVerSpec") {
-  property("when on v1.0.0 tag, w/o local changes") = passed
+  property("when on v1.0.0 tag, w/o local changes") = tagClean()
   property("when on v1.0.0 tag with local changes") = passed
   property("when on commit 1234abcd: 3 commits after v1.0.0 tag, w/o local changes") = passed
   property("when on commit 1234abcd: 3 commits after v1.0.0 tag with local changes") = passed
@@ -9,4 +16,21 @@ object DynVerSpec extends Properties("DynVerSpec") {
   property("when there are no tags, on commit 1234abcd with local changes") = passed
   property("when there are no commits") = passed
   property("when not a git repo") = passed
+
+  def tagClean(): Prop = {
+    val dir = Files.createTempDirectory("dynver-test-tag-clean-").toFile
+    dir.deleteOnExit()
+
+    val git = Git.init().setDirectory(dir).call()
+
+    val file = dir.toPath.resolve("f.txt")
+
+    Files.write(file, Seq("1").asJava, CREATE, APPEND)
+
+    git.commit().setMessage("1").call()
+
+    git.tag().setName("v1.0.0").setAnnotated(true).call()
+
+    DynVer(Some(dir)).version() ?= "1.0.0"
+  }
 }
