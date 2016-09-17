7188b68451fdcea30aa10f400e5d6a2fc3972b90
Dale Wijnand <dale.wijnand@gmail.com>
2016-09-17 12:32:37 +0100

    Setup test stubs

--- build.sbt
+++ build.sbt
@@ -24,6 +24,9 @@ scalacOptions  += "-Ywarn-dead-code"
 scalacOptions  += "-Ywarn-numeric-widen"
 scalacOptions  += "-Ywarn-value-discard"
 
+libraryDependencies += "org.eclipse.jgit"  % "org.eclipse.jgit" % "4.4.1.201607150455-r" % Test
+libraryDependencies += "org.scalacheck"   %% "scalacheck"       % "1.13.2"               % Test
+
              fork in Test := false
       logBuffered in Test := false
 parallelExecution in Test := true
---
+++ src/test/scala/DynverSpec.scala
@@ -0,0 +1,12 @@
+import org.scalacheck._, Prop._
+
+object DynverSpec extends Properties("DynverSpec") {
+  property("when on v1.0.0 tag, w/o local changes") = passed
+  property("when on v1.0.0 tag with local changes") = passed
+  property("when on commit 1234abcd: 3 commits after v1.0.0 tag, w/o local changes") = passed
+  property("when on commit 1234abcd: 3 commits after v1.0.0 tag with local changes") = passed
+  property("when there are no tags, on commit 1234abcd, w/o local changes") = passed
+  property("when there are no tags, on commit 1234abcd with local changes") = passed
+  property("when there are no commits") = passed
+  property("when not a git repo") = passed
+}
