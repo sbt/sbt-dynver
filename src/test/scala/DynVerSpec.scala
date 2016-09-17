a4c12a9812c8e17ee65d98e591d4c7afe76427c5
Dale Wijnand <dale.wijnand@gmail.com>
2016-09-17 17:13:57 +0100

    Add IsSnapshotSpec

--- src/test/scala/DynVerSpec.scala
+++ src/test/scala/DynVerSpec.scala
@@ -8,7 +8,7 @@ import org.eclipse.jgit.api._
 import sbtdynver._
 import RepoStates._
 
-object DynVerSpec extends Properties("DynVerSpec") {
+object VersionSpec extends Properties("VersionSpec") {
   property("not a git repo")                                = notAGitRepo().version()         ?= "HEAD+20160917"
   property("no commits")                                    = noCommits().version()           ?= "HEAD+20160917"
   property("on commit, w/o local changes")                  = onCommit().version()            ?= "1234abcd"
@@ -17,6 +17,17 @@ object DynVerSpec extends Properties("DynVerSpec") {
   property("on tag v1.0.0 with local changes")              = onTagDirty().version()          ?= "1.0.0+20160917"
   property("on tag v1.0.0 and 1 commit, w/o local changes") = onTagAndCommit().version()      ?= "1.0.0+1-1234abcd"
   property("on tag v1.0.0 and 1 commit with local changes") = onTagAndCommitDirty().version() ?= "1.0.0+1-1234abcd+20160917"
+}
+
+object IsSnapshotSpec extends Properties("IsSnapshotSpec") {
+//  property("not a git repo")                                = notAGitRepo().isSnapshot()         ?= true
+  property("no commits")                                    = noCommits().isSnapshot()           ?= true
+  property("on commit, w/o local changes")                  = onCommit().isSnapshot()            ?= true
+  property("on commit with local changes")                  = onCommitDirty().isSnapshot()       ?= true
+  property("on tag v1.0.0, w/o local changes")              = onTag().isSnapshot()               ?= false
+  property("on tag v1.0.0 with local changes")              = onTagDirty().isSnapshot()          ?= true
+  property("on tag v1.0.0 and 1 commit, w/o local changes") = onTagAndCommit().isSnapshot()      ?= false
+  property("on tag v1.0.0 and 1 commit with local changes") = onTagAndCommitDirty().isSnapshot() ?= true
 }
 
 object RepoStates {
