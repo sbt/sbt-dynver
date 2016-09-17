3f809d974dab789f32ab1d0d283222c8ad4e5c65
Dale Wijnand <dale.wijnand@gmail.com>
2016-09-17 16:52:59 +0100

    Move to State class

--- src/test/scala/DynVerSpec.scala
+++ src/test/scala/DynVerSpec.scala
@@ -1,5 +1,4 @@
 import java.nio.file._, StandardOpenOption._
-import java.io._
 import java.util.{ Properties => _, _ }
 
 import scala.collection.JavaConverters._
@@ -9,96 +8,52 @@ import org.eclipse.jgit.api._
 import sbtdynver._
 
 object DynVerSpec extends Properties("DynVerSpec") {
-  property("not a git repo") = noGitRepo()
+  property("not a git repo") = notAGitRepo()
   property("no commits") = noCommits()
-  property("on commit, w/o local changes") = noTagsClean()
-  property("on commit with local changes") = noTagsDirty()
-  property("on tag v1.0.0, w/o local changes") = tagClean()
-  property("on tag v1.0.0 with local changes") = tagDirty()
-  property("on 1 commit after v1.0.0 tag, w/o local changes") = tagChangesClean()
-  property("on 1 commit after v1.0.0 tag with local changes") = tagChangesDirty()
+  property("on commit, w/o local changes") = onCommit()
+  property("on commit with local changes") = onCommitDirty()
+  property("on tag v1.0.0, w/o local changes") = onTag()
+  property("on tag v1.0.0 with local changes") = onTagDirty()
+  property("on tag v1.0.0 and 1 commit, w/o local changes") = onTagAndCommit()
+  property("on tag v1.0.0 and 1 commit with local changes") = onTagAndCommitDirty()
 
-  def noGitRepo(): Prop = versionAtDir(createTempDir()) ?= "HEAD+20160917"
-  def noCommits(): Prop = version(newRepo())            ?= "HEAD+20160917"
+  def notAGitRepo(): Prop = State().version()        ?= "HEAD+20160917"
+  def noCommits(): Prop   = State().init().version() ?= "HEAD+20160917"
 
-  def noTagsClean(): Prop = {
-    val git = newRepo()
-    writeToFile(git)
-    val sha = commit(git)
-    version(git) ?= sha
-  }
+  def onCommit(): Prop = State().init().commit().version() ?= "1234abcd"
 
-  def noTagsDirty(): Prop = {
-    val git = newRepo()
-    writeToFile(git)
-    val sha = commit(git)
-    writeToFile(git)
-    version(git) ?= s"$sha+20160917"
-  }
+  def onCommitDirty(): Prop = State().init().commit().dirty().version() ?= "1234abcd+20160917"
 
-  def tagClean(): Prop = {
-    val git = newRepo()
-    writeToFile(git)
-    commit(git)
-    tag(git)
-    version(git) ?= "1.0.0"
-  }
+  def onTag(): Prop = State().init().commit().tag().version() ?= "1.0.0"
 
-  def tagDirty(): Prop = {
-    val git = newRepo()
-    writeToFile(git)
-    commit(git)
-    tag(git)
-    writeToFile(git)
-    version(git) ?= "1.0.0+20160917"
-  }
+  def onTagDirty(): Prop = State().init().commit().tag().dirty().version() ?= "1.0.0+20160917"
 
-  def tagChangesClean(): Prop = {
-    val git = newRepo()
-    writeToFile(git)
-    commit(git)
-    tag(git)
-    writeToFile(git)
-    val sha = commit(git)
-    version(git) ?= s"1.0.0+1-$sha"
-  }
+  def onTagAndCommit(): Prop = State().init().commit().tag().commit().version() ?= "1.0.0+1-1234abcd"
 
-  def tagChangesDirty(): Prop = {
-    val git = newRepo()
-    writeToFile(git)
-    commit(git)
-    tag(git)
-    writeToFile(git)
-    val sha = commit(git)
-    writeToFile(git)
-    version(git) ?= s"1.0.0+1-$sha+20160917"
-  }
+  def onTagAndCommitDirty(): Prop = State().init().commit().tag().commit().dirty().version() ?= "1.0.0+1-1234abcd+20160917"
 
-
-  private def newRepo() = Git.init().setDirectory(createTempDir()).call()
-
-  def writeToFile(git: Git) = {
-    val file = git.getRepository.getWorkTree.toPath.resolve("f.txt")
-    Files.write(file, Seq("1").asJava, CREATE, APPEND)
-  }
-
-  private def commit(git: Git) = {
-    git.add().addFilepattern(".").call()
-    val commit = git.commit().setMessage("1").call()
-    commit.abbreviate(8).name()
-  }
-
-  def tag(git: Git) =  git.tag().setName("v1.0.0").setAnnotated(true).call()
-
-  private def version(git: Git) = versionAtDir(git.getRepository.getWorkTree)
-
-  private def versionAtDir(dir: File) = DynVer(Some(dir), fakeClock).version()
-
-  private def createTempDir() = {
+  final case class State() {
     val dir = Files.createTempDirectory(s"dynver-test-").toFile
     dir.deleteOnExit()
-    dir
-  }
 
-  private val fakeClock = FakeClock(new GregorianCalendar(2016, 9, 17).getTime)
+    val fakeClock = FakeClock(new GregorianCalendar(2016, 9, 17).getTime)
+    val dynver = DynVer(Some(dir), fakeClock)
+
+    var git: Git = _
+    var sha: String = "undefined"
+
+    def init()  = andThis(git = Git.init().setDirectory(dir).call())
+    def dirty() = andThis(Files.write(dir.toPath.resolve("f.txt"), Seq("1").asJava, CREATE, APPEND))
+    def tag()   = andThis(git.tag().setName("v1.0.0").setAnnotated(true).call())
+
+    def commit() = andThis {
+      dirty()
+      git.add().addFilepattern(".").call()
+      sha = git.commit().setMessage("1").call().abbreviate(8).name()
+    }
+
+    def version() = dynver.version().replaceAllLiterally(sha, "1234abcd")
+
+    private def andThis[U](x: U): this.type = this
+  }
 }
