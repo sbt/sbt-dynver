8df545d0d3498c258858b2c28f87a2289b9de307
Dale Wijnand <dale.wijnand@gmail.com>
2016-09-30 02:32:29 +0100

    Add GitDescribeOutput for structured data from 1 git call

--- src/main/scala/sbtdynver/DynVerPlugin.scala
+++ src/main/scala/sbtdynver/DynVerPlugin.scala
@@ -1,9 +1,7 @@
 package sbtdynver
 
 import java.util._
-
 import scala.util._
-
 import sbt._, Keys._
 
 object DynVerPlugin extends AutoPlugin {
@@ -11,56 +9,101 @@ object DynVerPlugin extends AutoPlugin {
   override def trigger  = allRequirements
 
   object autoImport {
-    val dynver              = taskKey[String]("The version of your project, from git")
-    val dynverCurrentDate   = settingKey[Date]("The current date, for dynver purposes")
-    val dynverCheckVersion  = taskKey[Boolean]("Checks if version and dynver match")
-    val dynverAssertVersion = taskKey[Unit]("Asserts if version and dynver match")
+    val dynver                  = taskKey[String]("The version of your project, from git")
+    val dynverCurrentDate       = settingKey[Date]("The current date, for dynver purposes")
+    val dynverGitDescribeOutput = settingKey[Option[GitDescribeOutput]]("The output from git describe")
+    val dynverCheckVersion      = taskKey[Boolean]("Checks if version and dynver match")
+    val dynverAssertVersion     = taskKey[Unit]("Asserts if version and dynver match")
   }
   import autoImport._
 
   override def buildSettings = Seq(
-       version := DynVer(None).version(dynverCurrentDate.value),
-    isSnapshot := DynVer(None).isSnapshot(),
+       version := dynverGitDescribeOutput.value.version(dynverCurrentDate.value),
+    isSnapshot := dynverGitDescribeOutput.value.isSnapshot,
 
-    dynver              := DynVer(None).version(new Date),
-    dynverCurrentDate   := new Date,
-    dynverCheckVersion  := (dynver.value == version.value),
-    dynverAssertVersion := (if (!dynverCheckVersion.value)
-      sys.error(s"Version and dynver mismatch - version: ${version.value}, dynver: ${dynver.value}")
+    dynverCurrentDate       := new Date,
+    dynverGitDescribeOutput := DynVer(None).getGitDescribeOutput(dynverCurrentDate.value),
+
+    dynver                  := DynVer(None).version(new Date),
+    dynverCheckVersion      := (dynver.value == version.value),
+    dynverAssertVersion     := (
+      if (!dynverCheckVersion.value)
+        sys.error(s"Version and dynver mismatch - version: ${version.value}, dynver: ${dynver.value}")
     )
   )
 }
 
-final case class DynVer(wd: Option[File]) {
-  def version(d: Date): String = makeDynVer(d) getOrElse s"HEAD+${timestamp(d)}"
-  def isSnapshot(): Boolean    = isDirty() || hasNoTags()
+final case class GitRef(value: String)
+final case class GitCommitSuffix(distance: Int, sha: String)
+final case class GitDirtySuffix(value: String)
 
-  def timestamp(d: Date): String = "%1$tY%1$tm%1$td-%1$tH%1$tM" format d
+final case class GitDescribeOutput(ref: GitRef, commitSuffix: GitCommitSuffix, dirtySuffix: GitDirtySuffix) {
+  import commitSuffix.{ distance, sha }
+  private def comStr = if (distance <= 0 || sha.isEmpty) "" else s"+$distance-$sha"
 
-  def makeDynVer(d: Date): Option[String] = {
-    val tstamp = timestamp(d)
-    Try(
-      Process(s"""git describe --abbrev=8 --match v[0-9].* --always --dirty=+$tstamp""", wd).!!(NoProcessLogger)
-    ).toOption.map(_
-      .init
-      .replaceAll("^v", "")
-      .replaceAll("-([0-9]+)-g([0-9a-f]{8})", "+$1-$2")
-    )
+  def version: String       = ref.value.replaceAll("^v", "") + comStr + dirtySuffix.value
+  def isSnapshot(): Boolean = isDirty() || hasNoTags()
+
+  def isDirty(): Boolean    = dirtySuffix.value.nonEmpty
+  def hasNoTags(): Boolean  = !(ref.value startsWith "v")
+}
+
+object GitDescribeOutput extends ((GitRef, GitCommitSuffix, GitDirtySuffix) => GitDescribeOutput) {
+  private val Tag          =  """(v[0-9][^+]*)""".r
+  private val Distance     =  """\+([0-9]+)""".r
+  private val Sha          =  """([0-9a-f]{8})""".r
+  private val CommitSuffix = s"""($Distance-$Sha)""".r
+  private val TstampSuffix =  """(\+[0-9]{8}-[0-9]{4})""".r
+
+  private val FromTag  = s"""^$Tag$CommitSuffix?$TstampSuffix?$$""".r
+  private val FromSha  = s"""^$Sha$TstampSuffix?$$""".r
+  private val FromHead = s"""^HEAD$TstampSuffix$$""".r
+
+  private[sbtdynver] def parse(s: String): GitDescribeOutput = s match {
+    case FromTag(tag, _, dist, sha, dirty) => parse0(   tag, dist, sha, dirty)
+    case FromSha(sha, dirty)               => parse0(   sha,  "0",  "", dirty)
+    case FromHead(dirty)                   => parse0("HEAD",  "0",  "", dirty)
   }
 
-  def isDirty(): Boolean =
-    Try(Process("git status --untracked-files=no --porcelain", wd).!!(NoProcessLogger))
-      .map(_.nonEmpty)
-      .getOrElse(true)
+  private def parse0(ref: String, dist: String, sha: String, dirty: String) = {
+    val commit = if (dist == null || sha == null) GitCommitSuffix(0, "") else GitCommitSuffix(dist.toInt, sha)
+    GitDescribeOutput(GitRef(ref), commit, GitDirtySuffix(if (dirty eq null) "" else dirty))
+  }
 
-  def hasNoTags(): Boolean =
-    Try(Process("git for-each-ref --format %(objecttype) refs/tags/", wd).!!(NoProcessLogger))
-      .map(_.linesIterator.forall(_ startsWith "commit"))
-      .getOrElse(true)
+  private[sbtdynver] implicit class OptGitDescribeOutputOps(val _x: Option[GitDescribeOutput]) extends AnyVal {
+    def version(d: Date): String = _x.fold(s"HEAD+${timestamp(d)}")(_.version)
+    def isSnapshot: Boolean      = _x.fold(true)(x => x.isDirty() || x.hasNoTags())
+
+    def isDirty: Boolean         = _x.fold(true)(_.isDirty())
+    def hasNoTags: Boolean       = _x.fold(true)(_.hasNoTags())
+  }
+}
+
+final case class DynVer(wd: Option[File]) {
+  def version(d: Date): String            = getGitDescribeOutput(d) version d
+  def isSnapshot(): Boolean               = getGitDescribeOutput(new Date).isSnapshot
+
+  def makeDynVer(d: Date): Option[String] = getGitDescribeOutput(d) map (_.version)
+  def isDirty(): Boolean                  = getGitDescribeOutput(new Date).isDirty
+  def hasNoTags(): Boolean                = getGitDescribeOutput(new Date).hasNoTags
+
+  private[sbtdynver] def getGitDescribeOutput(d: Date) = {
+    val process = Process(s"""git describe --abbrev=8 --match v[0-9].* --always --dirty=+${timestamp(d)}""", wd)
+    Try(process !! NoProcessLogger).toOption
+      .map(_.init)
+      .map(_.replaceAll("-([0-9]+)-g([0-9a-f]{8})", "+$1-$2"))
+      .map(GitDescribeOutput.parse)
+  }
+
+  def timestamp(d: Date): String = sbtdynver timestamp d
+}
+
+object `package` {
+  private[sbtdynver] def timestamp(d: Date): String = "%1$tY%1$tm%1$td-%1$tH%1$tM" format d
 }
 
 object NoProcessLogger extends ProcessLogger {
-  def info(s: =>String)  = ()
-  def error(s: =>String) = ()
-  def buffer[T](f: => T) = f
+  def info(s: => String)  = ()
+  def error(s: => String) = ()
+  def buffer[T](f: => T)  = f
 }
