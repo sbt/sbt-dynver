16148bf53a82177aff976fba485702ca5a7145fb
Dale Wijnand <dale.wijnand@gmail.com>
2016-11-19 19:02:30 +0000

    Split out RepoStates

--- src/test/scala/sbtdynver/DynVerSpec.scala
+++ src/test/scala/sbtdynver/DynVerSpec.scala
@@ -1,12 +1,6 @@
 package sbtdynver
 
-import java.nio.file._, StandardOpenOption._
-import java.util.{ Properties => _, _ }
-
-import scala.collection.JavaConverters._
-
 import org.scalacheck._, Prop._
-import org.eclipse.jgit.api._
 
 import RepoStates._
 
@@ -30,41 +24,4 @@ object IsSnapshotSpec extends Properties("IsSnapshotSpec") {
   property("on tag v1.0.0 with local changes")              = onTagDirty().isSnapshot()          ?= true
   property("on tag v1.0.0 and 1 commit, w/o local changes") = onTagAndCommit().isSnapshot()      ?= false
   property("on tag v1.0.0 and 1 commit with local changes") = onTagAndCommitDirty().isSnapshot() ?= true
-}
-
-object RepoStates {
-  def notAGitRepo()         = State()
-  def noCommits()           = notAGitRepo().init()
-  def onCommit()            = noCommits().commit()
-  def onCommitDirty()       = onCommit().dirty()
-  def onTag()               = onCommit().tag()
-  def onTagDirty()          = onTag().dirty()
-  def onTagAndCommit()      = onTag().commit()
-  def onTagAndCommitDirty() = onTagAndCommit().dirty()
-
-  final case class State() {
-    val dir = doto(Files.createTempDirectory(s"dynver-test-").toFile)(_.deleteOnExit())
-    val date = new GregorianCalendar(2016, 8, 17).getTime
-    val dynver = DynVer(Some(dir))
-
-    var git: Git = _
-    var sha: String = "undefined"
-
-    def init()  = andThis(git = Git.init().setDirectory(dir).call())
-    def dirty() = andThis(Files.write(dir.toPath.resolve("f.txt"), Seq("1").asJava, CREATE, APPEND))
-    def tag()   = andThis(git.tag().setName("v1.0.0").setAnnotated(true).call())
-
-    def commit() = andThis {
-      dirty()
-      git.add().addFilepattern(".").call()
-      sha = git.commit().setMessage("1").call().abbreviate(8).name()
-    }
-
-    def version()    = dynver.version(date).replaceAllLiterally(sha, "1234abcd")
-    def isSnapshot() = dynver.isSnapshot()
-
-    private def doalso[A, U](x: A)(xs: U*)  = x
-    private def doto[A, U](x: A)(f: A => U) = doalso(x)(f(x))
-    private def andThis[U](x: U)            = this
-  }
 }
