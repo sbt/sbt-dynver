c00b8ff47b7f7f1ec84002fe20c5e8568d5c499c
Dale Wijnand <dale.wijnand@gmail.com>
2016-11-19 21:51:21 +0000

    Re-enable Travis CI to publish on tags
    Fixes #17

--- project/travis.sh
+++ project/travis.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-if [[ "$TRAVIS_PULL_REQUEST" == "false"
-   && "$TRAVIS_BRANCH" == "master"
-   && "$TRAVIS_SECURE_ENV_VARS" == "true"
-]]; then
+[[ "$TRAVIS_PULL_REQUEST" == "false"
+&& "$TRAVIS_BRANCH" == "master"
+&& "$TRAVIS_SECURE_ENV_VARS" == "true"
+]]
+on_the_master_branch=$?
+
+[[ "$TRAVIS_TAG" != ""
+&& "$TRAVIS_SECURE_ENV_VARS" == "true"
+]]
+on_a_tag=$?
+
+if [[ $on_the_master_branch == 0 || $on_a_tag == 0 ]]; then
   PUBLISH=publish
   mkdir ~/.bintray
   cat > ~/.bintray/.credentials <<EOF
