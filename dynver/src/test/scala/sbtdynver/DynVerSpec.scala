29773971eb2072d84e4e295e40771639e7ba014c
Dale Wijnand <dale.wijnand@gmail.com>
2021-01-14 08:44:37 +0000

    Cleanup tests

--- dynver/src/test/scala/sbtdynver/DynVerSpec.scala
+++ dynver/src/test/scala/sbtdynver/DynVerSpec.scala
@@ -5,126 +5,62 @@ import org.scalacheck._, Prop._
 import RepoStates._
 
 object VersionSpec extends Properties("VersionSpec") {
-  property("not a git repo")                                = notAGitRepo().version()         ?= "HEAD+20160917-0000"
-  property("no commits")                                    = noCommits().version()           ?= "HEAD+20160917-0000"
-  property("on commit, w/o local changes")                  = onCommit().version()            ?= "0.0.0+3-1234abcd"
-  property("on commit with local changes")                  = onCommitDirty().version()       ?= "0.0.0+3-1234abcd+20160917-0000"
-  property("on tag v1.0.0, w/o local changes")              = onTag().version()               ?= "1.0.0"
-  property("on tag v1.0.0 with local changes")              = onTagDirty().version()          ?= "1.0.0+0-1234abcd+20160917-0000"
-  property("on tag v1.0.0 and 1 commit, w/o local changes") = onTagAndCommit().version()      ?= "1.0.0+1-1234abcd"
-  property("on tag v1.0.0 and 1 commit with local changes") = onTagAndCommitDirty().version() ?= "1.0.0+1-1234abcd+20160917-0000"
-  property("on tag v2")                                     = onTag("v2").version()           ?= "2" // #7, didn't match
+  property("not a git repo")                                = notAGitRepo          .version ?= "HEAD+20160917-0000"
+  property("no commits")                                    = noCommits            .version ?= "HEAD+20160917-0000"
+  property("on commit, w/o local changes")                  = onCommit             .version ?= "0.0.0+3-1234abcd"
+  property("on commit with local changes")                  = onCommit.dirty       .version ?= "0.0.0+3-1234abcd+20160917-0000"
+  property("on tag v1.0.0, w/o local changes")              = onTag                .version ?= "1.0.0"
+  property("on tag v1.0.0 with local changes")              = onTag.dirty          .version ?= "1.0.0+0-1234abcd+20160917-0000"
+  property("on tag v1.0.0 and 1 commit, w/o local changes") = onTag.commit         .version ?= "1.0.0+1-1234abcd"
+  property("on tag v1.0.0 and 1 commit with local changes") = onTag.commit.dirty   .version ?= "1.0.0+1-1234abcd+20160917-0000"
+  property("on tag v2")                                     = onTag.commit.tag("2").version ?= "2" // #7, didn't match
 }
 
 object PreviousVersionSpec extends Properties("PreviousVersionSpec") {
-  property("not a git repo")                                  = notAGitRepo().previousVersion()             ?= None
-  property("no commits")                                      = noCommits().previousVersion()               ?= None
-  property("on commit, w/o local changes")                    = onCommit().previousVersion()                ?= None
-  property("on commit with local changes")                    = onCommitDirty().previousVersion()           ?= None
-  property("on tag v1.0.0, w/o local changes")                = onTag().previousVersion()                   ?= None
-  property("on tag v1.0.0 with local changes")                = onTagDirty().previousVersion()              ?= None
-  property("on tag v1.0.0 and 1 commit, w/o local changes")   = onTagAndCommit().previousVersion()          ?= Some("1.0.0")
-  property("on tag v1.0.0 and 1 commit with local changes")   = onTagAndCommitDirty().previousVersion()     ?= Some("1.0.0")
-  property("on tag v1.0.0 and 2 commits, w/o local changes")  = onTagAndSecondCommit().previousVersion()    ?= Some("1.0.0")
-  property("on tag v2.0.0, w/o local changes")                = onSecondTag().previousVersion()             ?= Some("1.0.0")
-  property("with merge commits") = {
-    val state = onTag()
-      .branch("newbranch")
-      .commit()
-      .checkout("master")
-      .merge("newbranch")
-      .tag("v2.0.0")
+  property("not a git repo")                                = notAGitRepo              .previousVersion ?= None
+  property("no commits")                                    = noCommits                .previousVersion ?= None
+  property("on commit, w/o local changes")                  = onCommit                 .previousVersion ?= None
+  property("on commit with local changes")                  = onCommit.dirty           .previousVersion ?= None
+  property("on tag v1.0.0, w/o local changes")              = onTag                    .previousVersion ?= None
+  property("on tag v1.0.0 with local changes")              = onTag.dirty              .previousVersion ?= None
+  property("on tag v1.0.0 and 1 commit, w/o local changes") = onTag.commit             .previousVersion ?= Some("1.0.0")
+  property("on tag v1.0.0 and 1 commit with local changes") = onTag.commit.dirty       .previousVersion ?= Some("1.0.0")
+  property("on tag v2.0.0, w/o local changes")              = onTag.commit.tag("2.0.0").previousVersion ?= Some("1.0.0")
 
-    state.previousVersion() ?= Some("1.0.0")
-  }
-
-  property("with merge commits and with untagged HEAD") = {
-    val state = onTag()
-      .branch("newbranch")
-      .commit()
-      .checkout("master")
-      .merge("newbranch")
-
-    state.previousVersion() ?= Some("1.0.0")
-  }
-
-  property("multiple branches, each with their own tags") = {
-    val state = onTag()
-
-    state
-      .branch("v2")
-      .commit()
-      .tag("v2.0.0")
-
-    state
-      .commit()
-      .tag("v2.1.0")
-
-    state
-      .checkout("master") // checkout the v1.x branch
-      .commit()
-      .tag("v1.1.0")
-
-    state.checkout("v2")
-
-    state.previousVersion() ?= Some("2.0.0")
-  }
-
-  property("merge commit where both branches have tags - should use the first parent (branch that was merged into)") = {
-    val state = onTag()
-
-    state
-      .branch("v2")
-      .commit()
-      .tag("v2.0.0")
-
-    state
-      .commit()
-      .tag("v2.1.0")
-
-    state
-      .checkout("master") // checkout the v1.x branch
-      .commit()
-      .tag("v1.1.0")
-
-    state.merge("v2")
-
-    state.previousVersion() ?= Some("1.1.0")
-  }
-
-  property("contains unrelated tags in-between version tags") = {
-    val state = onTag().commit().tag("unrelated").commit().tag("v2.0.0")
-    state.previousVersion() ?= Some("1.0.0")
-  }
+  property("w/ merge commits")            = onBranch2x.checkout("master").merge("2.x") .previousVersion ?= Some("1.0.0")
+  property("w/ merge commits + tag")      = onBranch2Tag                               .previousVersion ?= Some("1.0.0")
+  property("on multiple branches")        = onMultiBranch.checkout("2.x")              .previousVersion ?= Some("2.0.0")
+  property("on merging branch with tags") = onMultiBranch.merge("2.x")                 .previousVersion ?= Some("1.1.0")
+  property("with non-version prev tag")   = onTag.commit.tag("hm").commit.tag("v2.0.0").previousVersion ?= Some("1.0.0")
 }
 
 object IsSnapshotSpec extends Properties("IsSnapshotSpec") {
-  property("not a git repo")                                = notAGitRepo().isSnapshot()         ?= true
-  property("no commits")                                    = noCommits().isSnapshot()           ?= true
-  property("on commit, w/o local changes")                  = onCommit().isSnapshot()            ?= true
-  property("on commit with local changes")                  = onCommitDirty().isSnapshot()       ?= true
-  property("on tag v1.0.0, w/o local changes")              = onTag().isSnapshot()               ?= false
-  property("on tag v1.0.0 with local changes")              = onTagDirty().isSnapshot()          ?= true
-  property("on tag v1.0.0 and 1 commit, w/o local changes") = onTagAndCommit().isSnapshot()      ?= true
-  property("on tag v1.0.0 and 1 commit with local changes") = onTagAndCommitDirty().isSnapshot() ?= true
-  property("on tag v2")                                     = onTag("v2").isSnapshot()           ?= false
+  property("not a git repo")                                = notAGitRepo          .isSnapshot ?= true
+  property("no commits")                                    = noCommits            .isSnapshot ?= true
+  property("on commit, w/o local changes")                  = onCommit             .isSnapshot ?= true
+  property("on commit with local changes")                  = onCommit.dirty       .isSnapshot ?= true
+  property("on tag v1.0.0, w/o local changes")              = onTag                .isSnapshot ?= false
+  property("on tag v1.0.0 with local changes")              = onTag.dirty          .isSnapshot ?= true
+  property("on tag v1.0.0 and 1 commit, w/o local changes") = onTag.commit         .isSnapshot ?= true
+  property("on tag v1.0.0 and 1 commit with local changes") = onTag.commit.dirty   .isSnapshot ?= true
+  property("on tag v2")                                     = onTag.commit.tag("2").isSnapshot ?= false
 }
 
 object SonatypeSnapshotSpec extends Properties("SonatypeSnapshotSpec") {
-  property("on tag v1.0.0 with local changes")                = onTagDirty().sonatypeVersion()     ?= "1.0.0+0-1234abcd+20160917-0000-SNAPSHOT"
-  property("on tag v1.0.0 and 1 commit, w/o local changes S") = onTagAndCommit().sonatypeVersion() ?= "1.0.0+1-1234abcd-SNAPSHOT"
-  property("on tag v1.0.0, w/o local changes")                = onTag().sonatypeVersion()          ?= "1.0.0"
-  property("on tag v2")                                     = onTag("v2").sonatypeVersion()        ?= "2"
+  property("on tag v1.0.0 with local changes")                = onTag.dirty          .sonatypeVersion ?= "1.0.0+0-1234abcd+20160917-0000-SNAPSHOT"
+  property("on tag v1.0.0 and 1 commit, w/o local changes S") = onTag.commit         .sonatypeVersion ?= "1.0.0+1-1234abcd-SNAPSHOT"
+  property("on tag v1.0.0, w/o local changes")                = onTag                .sonatypeVersion ?= "1.0.0"
+  property("on tag v2")                                       = onTag.commit.tag("2").sonatypeVersion ?= "2"
 }
 
 object isVersionStableSpec extends Properties("IsVersionStableSpec") {
-  property("not a git repo")                                = notAGitRepo().isVersionStable()         ?= false
-  property("no commits")                                    = noCommits().isVersionStable()           ?= false
-  property("on commit, w/o local changes")                  = onCommit().isVersionStable()            ?= true
-  property("on commit with local changes")                  = onCommitDirty().isVersionStable()       ?= false
-  property("on tag v1.0.0, w/o local changes")              = onTag().isVersionStable()               ?= true
-  property("on tag v1.0.0 with local changes")              = onTagDirty().isVersionStable()          ?= false
-  property("on tag v1.0.0 and 1 commit, w/o local changes") = onTagAndCommit().isVersionStable()      ?= true
-  property("on tag v1.0.0 and 1 commit with local changes") = onTagAndCommitDirty().isVersionStable() ?= false
-  property("on tag v2")                                     = onTag("v2").isVersionStable()           ?= true
+  property("not a git repo")                                = notAGitRepo          .isVersionStable ?= false
+  property("no commits")                                    = noCommits            .isVersionStable ?= false
+  property("on commit, w/o local changes")                  = onCommit             .isVersionStable ?= true
+  property("on commit with local changes")                  = onCommit.dirty       .isVersionStable ?= false
+  property("on tag v1.0.0, w/o local changes")              = onTag                .isVersionStable ?= true
+  property("on tag v1.0.0 with local changes")              = onTag.dirty          .isVersionStable ?= false
+  property("on tag v1.0.0 and 1 commit, w/o local changes") = onTag.commit         .isVersionStable ?= true
+  property("on tag v1.0.0 and 1 commit with local changes") = onTag.commit.dirty   .isVersionStable ?= false
+  property("on tag v2")                                     = onTag.commit.tag("2").isVersionStable ?= true
 }
